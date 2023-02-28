4430afd9cd08a2412e369f57792fe24b45a36889
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2023-02-28 16:10:23 +0100

    test file for MQTT publish

---
+++ examples/test_MQTT/test_MQTT.ino
@@ -0,0 +1,34 @@
+#include <Quectel_BC660.h>
+
+#define SERIAL_PORT Serial2
+
+QuectelBC660 quectel = QuectelBC660(5, true);
+
+void setup() 
+{
+	Serial.begin(115200);
+	Serial.println("Quectel MQTT connection test");
+	Serial.println("===================");
+	quectel.begin(&SERIAL_PORT);
+    if(quectel.registered(5))
+    {
+        Serial.println("Module is registered to network");
+    }
+    quectel.setDeepSleep();
+    Serial.println("======MQTT SEND======");
+    quectel.openMQTT("ADDRESS OF MQTT BROKER");
+    delay(1000);
+    quectel.connectMQTT("Test-123456");
+    delay(1000);
+    quectel.publishMQTT("Hello world!", 12, "MQTT TOPIC");
+    delay(1000);
+    quectel.closeMQTT();
+    delay(1000);
+    Serial.println("======MQTT SEND DONE======");
+    quectel.setDeepSleep(1);
+}
+
+void loop()
+{
+
+}
